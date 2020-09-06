4deca6283ee72cfde33feae9399b23967d91f058
Lydia <lydia.chatziioannou@gmail.com>
2020-09-06 21:25:36 +0100

    className fix

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -32,9 +32,9 @@ class Loading extends Component {
     } else {
       return (
         <div>
-          <div class="d-flex justify-content-center">
-            <div class="spinner-border" role="status">
-              <span class="sr-only">Loading...</span>
+          <div className="d-flex justify-content-center">
+            <div className="spinner-border" role="status">
+              <span className="sr-only">Loading...</span>
             </div>
           </div>
         </div>
